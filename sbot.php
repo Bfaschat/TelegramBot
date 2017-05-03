0e5524a84a427a068e67b0db0f84cbf2738497e9
Mohammad Amin <mohammadaminonline@gmail.com>
2017-05-03 09:28:34 +0430

    Add Project

---
+++ sbot.php
@@ -0,0 +1,17 @@
+<?php
+
+$token ="347283471:AAFsJ2eptBct1nzmMQ1eLBB1ZYMqNLVlzcY";
+$website="https://api.telegram.org/bot".$token;
+
+$update =file_get_contents("php://input");
+$update =json_decode($update,TRUE);
+
+$text = $update ["message"]["text"];
+$chatID = $update["message"][0]["chat"]["id"];
+sendMessage($chatID,$text);
+function sendMessage($chatID,$message){
+  $url = $globals[website]."/sendMessage?chat_id=".$chatID."&text=.urlencode($message)";
+  file_get_contents($url);
+}
+
+ ?>
